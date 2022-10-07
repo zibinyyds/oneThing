d0d4c680f61adcc0ad577db5f390e845e3d4e5b8
zibin.liu@okg.com <zibin.liu@okg.com>
2022-10-07 20:34:11 +0800

    pwa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true,
+            },
+        }),
+    ],
 })
